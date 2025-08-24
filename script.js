a443fa9a6d27016ba2ad0310089e961ede4fcc01
Chetna Guptaa <74051849+ChetnaGupta-tech@users.noreply.github.com>
2025-08-24 16:51:30 +0530

    Updated script.js

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
-
+const 
 const playGame = (userChoice) =>{
     console.log("user choice= ",userChoice)
     //Generate computer choice
 }
+
 choices.forEach((choice) => {
     console.log(choice)
     choice.addEventListener("click", () => {
