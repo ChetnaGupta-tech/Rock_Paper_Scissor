04ebbbe2523b19ef74a7ab6ce53c065e7fe0aee1
Chetna Guptaa <74051849+ChetnaGupta-tech@users.noreply.github.com>
2025-08-24 17:00:10 +0530

    Updated computer choice function

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ let userScore = 0;
 let compScore = 0;
 const choices = document.querySelectorAll(".choice");
 
-const 
-const playGame = (userChoice) =>{
-    console.log("user choice= ",userChoice)
-    //Generate computer choice
-}
+const genCompChoice = () => {
+    const options = ["rock", "paper", "scissor"];
+    const ranIdx = Math.floor(Math.random() * 3); 
+    return options[ranIdx]; 
+};
+
+const playGame = (userChoice) => {
+    console.log("user choice = ", userChoice);
+    // Generate computer choice
+    const compChoice = genCompChoice();
+    console.log("comp choice = ", compChoice);
+};
 
 choices.forEach((choice) => {
-    console.log(choice)
+    console.log(choice);
     choice.addEventListener("click", () => {
-        const userChoice=choice.getAttribute("id")
-        // console.log("choice was clicked",userChoice)
-        playGame(userChoice)
+        const userChoice = choice.getAttribute("id");
+        playGame(userChoice);
     });
 });
