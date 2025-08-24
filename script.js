b7b99bf9ebca1f5cb04143bcc394883aad1c399a
Chetna Guptaa <74051849+ChetnaGupta-tech@users.noreply.github.com>
2025-08-24 16:50:10 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let userScore = 0;
+let compScore = 0;
+const choices = document.querySelectorAll(".choice");
+
+
+const playGame = (userChoice) =>{
+    console.log("user choice= ",userChoice)
+    //Generate computer choice
+}
+choices.forEach((choice) => {
+    console.log(choice)
+    choice.addEventListener("click", () => {
+        const userChoice=choice.getAttribute("id")
+        // console.log("choice was clicked",userChoice)
+        playGame(userChoice)
+    });
+});
